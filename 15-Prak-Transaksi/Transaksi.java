9afe6905f59669e2308d0970494cfc7d6a5a311b
muhammadyussuf <yussufm10@ymail.com>
2015-10-31 18:17:49 +0700

    Prak 15

---
+++ 15-Prak-Transaksi/Transaksi.java
@@ -0,0 +1,48 @@
+import java.util.Date;
+import java.math.BigInteger;
+
+public class Transaksi {
+    public Transaksi() {
+        this.kapan = new Date();
+        this.debit = new BigInteger("0");
+        this.kredit = new BigInteger("0");
+    }
+    
+    public Transaksi(Date kapan, long debit, long kredit) {
+        this.kapan = kapan;
+        this.debit = new BigInteger (Long.toString (debit));
+        this.kredit =new BigInteger (Long.toString (kredit));
+    }
+    
+    public Transaksi(Date kapan, String debit, String kredit) {
+        this.kapan = kapan;
+        this.debit = new BigInteger(debit);
+        this.kredit =new BigInteger (kredit);
+        }
+        
+        public Date kapan() {
+        return kapan;
+    }
+    
+    public BigInteger debit() {
+        return debit;
+    }
+    
+    public String debitStr() {
+        return debit.toString();
+    }
+    
+    public BigInteger kredit() {
+        return kredit;
+    }
+    
+    public String kreditStr() {
+        return kredit.toString();
+    }
+    
+    private Date kapan;
+    private BigInteger debit;
+    private BigInteger kredit;
+    
+    
+}
